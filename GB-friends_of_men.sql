709b8c4f08058665efed2611614a30476a633f73
Zhanaydarov Asset <asset0889@mail.ru>
2023-12-09 00:09:03 +0500

    задание 7-12

--- GB-friends_of_men.sql
+++ GB-friends_of_men.sql
@@ -1,137 +1,5 @@
-# Информация о проекте
+use friends_of_man;
 
-Необходимо организовать систему учета для питомника, в котором живут домашние и вьючные животные.
-
-# Задание
-
-1. Используя команду cat в терминале операционной системы Linux, создать
-два файла Домашние животные (заполнив файл собаками, кошками,
-хомяками) и Вьючные животными заполнив файл (Лошадьми, верблюдами и
-ослы), а затем объединить их. Просмотреть содержимое созданного файла.
-Переименовать файл, дав ему новое имя (Друзья человека).
-
-![Alt text](image.png)
-
-2. Создать директорию, переместить файл туда.
-
-![Alt text](image-1.png)
-
-3. Подключить дополнительный репозиторий MySQL. Установить любой пакет
-из этого репозитория.
-
-![Alt text](image-2.png)
-![Alt text](image-3.png)
-![Alt text](image-4.png)
-
-4. Установить и удалить deb-пакет с помощью dpkg.
-
-![Alt text](image-5.png)
-![Alt text](image-6.png)
-![Alt text](image-7.png)
-
-5. Выложить историю команд в терминале ubuntu
-
-asset_zh@DESKTOP-QAFSKRR:~$ history
-
-   21  ls -al
-  
-  22  cat > "Домашние_животные" <<EOF
-Собаки
-Кошки
-Хомяки
-EOF
-
-   23  ls -al
-   
-   24  nano Домашние_животные
-   
-   25  cat > "Вьючные_животные" <<EOF
-Лошади
-Верблюды
-Ослы
-EOF
-
-   26  nano Вьючные_животные
-   
-   27  cat Домашние_животные Вьючные_животные > animals
-   
-   28  ls -al
-   
-   29  nano animals
-   
-   30  mv animals mans_friends
-   
-   31  ls -ali
-   
-   32  cd mans_friends
-   
-   33  nano mans_friends
-   
-   36  mkdir animals
-   
-   37  mv mans_friends animals/
-   
-   38  ls -l
-   
-   39  cd animals
-   
-   40  ls -l
-   
-   41  sudo apt-get update
-   
-   42  sudo apt-get install software-properties-common
-   
-   43  sudo add-apt-repository -y ppa:mysql/mysql-server
-   
-   44  sudo add-apt-repository -y ppa:http://dev.mysql.com/
-   
-   45  sudo wget https://dev.mysql.com/get/mysql-apt-config_0.8.23-1_all.deb
-   
-   46  sudo dpkg -i mysql-apt-config_0.8.23-1_all.deb
-   
-   47  sudo apt-get update
-   
-   48  sudo apt-get install mysql-server
-   
-   49  cd ~
-   
-   50  sudo wget https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/docker-ce-cli_20.10.13~3-0~ubuntu-jammy_amd64.deb
-   
-   51  sudo dpkg -i docker-ce-cli_20.10.133-0ubuntu-jammy_amd64.deb
-   
-   61  sudo dpkg -i docker-ce-cli_20.10.133-0ubuntu-jammy_amd64.deb
-   
-   62  sudo wget https://download.docker.com/linux/ubuntu/dists/jammy/pool/stable/amd64/docker-ce-cli_20.10.13~3-0~ubuntu-jammy_amd64.deb
-   
-   63  ls -l
-   
-   64  sudo dpkg -i docker-ce-cli_20.10.133-0ubuntu-jammy_amd64.deb.1
-   
-   65  mv docker-ce-cli_20.10.13~3-0~ubuntu-jammy_amd64.deb ddocker.deb
-   
-   66  ls -l
-   
-   70  sudo dpkg -P docker-ce-cli
-   
-   71  history
-
-6. Нарисовать диаграмму, в которой есть класс родительский класс, домашние
-животные и вьючные животные, в составы которых в случае домашних
-животных войдут классы: собаки, кошки, хомяки, а в класс вьючные животные
-войдут: Лошади, верблюды и ослы.
-
-[Диаграма](\final_test\diagramm.drawio)
-
-7. В подключенном MySQL репозитории создать базу данных “Друзья
-человека”
-```sql
-CREATE DATABASE IF NOT EXISTS friends_of_man;
-USE friends_of_man;
-```
-
-8. Создать таблицы с иерархией из диаграммы в БД
-
-```sql
 CREATE TABLE IF NOT EXISTS animals(
 	Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     class_name VARCHAR(20)
@@ -140,6 +8,14 @@ CREATE TABLE IF NOT EXISTS animals(
 INSERT INTO animals(class_name) VALUES
 	("pets"),
     ("pack_animals");
+    
+CREATE TABLE pack_animals
+(
+	Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Genus_name VARCHAR (20),
+    Class_id INT,
+    FOREIGN KEY (Class_id) REFERENCES animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 CREATE TABLE pets
 (
@@ -154,26 +30,13 @@ VALUES
 	('Кошки', 1),
 	('Собаки', 1),  
 	('Хомяки', 1);
-
-CREATE TABLE pack_animals
-(
-	Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Genus_name VARCHAR (20),
-    Class_id INT,
-    FOREIGN KEY (Class_id) REFERENCES animals (Id) ON DELETE CASCADE ON UPDATE CASCADE
-);
     
 INSERT INTO pack_animals (Genus_name, Class_id)
 VALUES
 	('Лошади', 2),
 	('Ослы', 2),  
 	('Верблюды', 2); 
-
-
-```
-9. Заполнить низкоуровневые таблицы именами(животных), командами
-которые они выполняют и датами рождения
-```sql
+    
 CREATE TABLE cats 
 (       
     Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
@@ -183,7 +46,6 @@ CREATE TABLE cats
     Genus_id int,
     Foreign KEY (Genus_id) REFERENCES pets (Id) ON DELETE CASCADE ON UPDATE CASCADE
 );
-
 INSERT INTO cats (Name, Birthday, Commands, Genus_id)
 VALUES ('Мурка', '2022-01-01', 'кс-кс-кс', 1),
 ('Барсик', '2019-01-01', "кушать!", 1),  
@@ -263,21 +125,13 @@ VALUES ('Горбатый', '2022-04-10', 'вернись', 3),
 ('Самец', '2019-03-12', "остановись", 3),  
 ('Туйе', '2015-07-12', "повернись", 3), 
 ('Нар', '2022-12-10', "улыбнись", 3);
-```
-10. Удалив из таблицы верблюдов, т.к. верблюдов решили перевезти в другой
-питомник на зимовку. Объединить таблицы лошади, и ослы в одну таблицу.
-```sql
+
 SET SQL_SAFE_UPDATES = 0;
 DELETE FROM camels;
 
 SELECT Name, Birthday, Commands FROM horses
 UNION SELECT  Name, Birthday, Commands FROM donkeys;
-```
 
-11. Создать новую таблицу “молодые животные” в которую попадут все
-животные старше 1 года, но младше 3 лет и в отдельном столбце с точностью
-до месяца подсчитать возраст животных в новой таблице
-```sql
 CREATE TEMPORARY TABLE animals AS 
 SELECT *, 'Лошади' as genus FROM horses
 UNION SELECT *, 'Ослы' AS genus FROM donkeys
@@ -290,10 +144,7 @@ SELECT Name, Birthday, Commands, genus, TIMESTAMPDIFF(MONTH, Birthday, CURDATE()
 FROM animals WHERE Birthday BETWEEN ADDDATE(curdate(), INTERVAL -3 YEAR) AND ADDDATE(CURDATE(), INTERVAL -1 YEAR);
  
 SELECT * FROM yang_animal;
-```
-12. Объединить все таблицы в одну, при этом сохраняя поля, указывающие на
-прошлую принадлежность к старым таблицам.
-```sql
+
 SELECT h.Name, h.Birthday, h.Commands, pa.Genus_name, ya.Age_in_month 
 FROM horses h
 LEFT JOIN yang_animal ya ON ya.Name = h.Name
@@ -318,10 +169,4 @@ SELECT hm.Name, hm.Birthday, hm.Commands, ha.Genus_name, ya.Age_in_month
 FROM hamsters hm
 LEFT JOIN yang_animal ya ON ya.Name = hm.Name
 LEFT JOIN home_animals ha ON ha.Id = hm.Genus_id;
-```
-
-
-
-
-
-
+	
